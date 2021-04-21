ec7037dd6ace3b55a7b0b3b6c1727645970404ab
akalanka <sakalasooriyaakalanka@gmail.com>
2021-04-21 15:32:22 +0530

    forced merge akalanka to dev

---
+++ Authentication Service/REST/src/com/AuthenticationService.java
@@ -0,0 +1,97 @@
+package com;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.AuthenticationModel;
+
+@Path("/")
+public class AuthenticationService {
+	AuthenticationModel auth = new AuthenticationModel();
+
+	@GET
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String newLoginRequest(String data) {
+		JsonObject itemObject = new JsonParser().parse(data).getAsJsonObject();
+		String username = itemObject.get("username").getAsString();
+		String password = itemObject.get("password").getAsString();
+		return auth.userValidation(username, password);
+	}
+
+	// -----------------------------------------
+
+	@POST
+	@Path("/requestvalidator")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String requestvalidator(String data) {
+		JsonObject itemObject = new JsonParser().parse(data).getAsJsonObject();
+		String key = itemObject.get("key").getAsString();
+		String status = auth.requestValidation(key);
+		return "{validation_status:" + status + "}";
+	}
+
+	// -------------------------------------------
+
+	@DELETE
+	@Path("/logout")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String logOut(String data) {
+		JsonObject itemObject = new JsonParser().parse(data).getAsJsonObject();
+		String key = itemObject.get("key").getAsString();
+		String status = auth.logOut(key);
+		return "{status:" + status + "}";
+	}
+
+	// -------------------------------------------
+
+	@PUT
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String credentialUpdate(String data) {
+		String status = "error";
+		JsonObject itemObject = new JsonParser().parse(data).getAsJsonObject();
+		String key = itemObject.get("key").getAsString();
+		String id = itemObject.get("user_id").getAsString();
+		String username = itemObject.get("username").getAsString();
+		String password = itemObject.get("password").getAsString();
+		// validating request
+		if (auth.requestValidation(key).equals("valid")) {
+			status = auth.credentialUpdate(Integer.valueOf(id), username, password);
+		}
+		return "{status:" + status + "}";
+	}
+
+	// -------------------------------------------
+
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String addUser(String data) {
+		JsonObject itemObject = new JsonParser().parse(data).getAsJsonObject();
+		String status = "error";
+		String key = itemObject.get("key").getAsString();
+		int id = itemObject.get("user_id").getAsInt();
+		String username = itemObject.get("username").getAsString();
+		String password = itemObject.get("password").getAsString();
+
+		if (auth.requestValidation(key).equals("valid")) {
+			status = auth.addUser(id, username, password);
+		}
+		return "{status:" + status + "}";
+	}
+}
--- Product Service/REST/src/model/ProductManagementModel.java
+++ Product Service/REST/src/model/ProductManagementModel.java
@@ -3,10 +3,6 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 public class ProductManagementModel {
 	
@@ -23,7 +19,7 @@ public class ProductManagementModel {
 		return con;
 	}
 	
-	public boolean addProducts (int research_id, String name, String description, int stock_quantity, double price, String added_date) {
+	public boolean addProducts (int products_id, int research_id, String name, String description, int stock_quantity, double price, String added_date) {
 
 		try {
 
@@ -34,11 +30,11 @@ public class ProductManagementModel {
 			}
 			
 			// create a prepared statement
-			String query =  " insert into products (`research_id`,`name`,`description`,`stock_quantity`,`price`,`added_date`) values (?, ?, ?, ?, ?, ?)"; 
+			String query = " insert into products values(? , ? , ? , ? , ? , ? , ?) ";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
 
-			preparedStmt.setInt(1, 0);
+			preparedStmt.setInt(1, products_id);
 			preparedStmt.setInt(2, research_id);
 			preparedStmt.setString(3, name);
 			preparedStmt.setString(4, description);
@@ -58,158 +54,7 @@ public class ProductManagementModel {
 	}
 	
 	
-	public String readProducts() {
-		JsonObject products = new JsonObject();
-
-		try {
-
-			Connection con = connect();
-			if (con == null) {
-				return products.toString();
-			}
-			// create a prepared statement
-			String query = " select * from products ";
-			PreparedStatement preparedStmt = con.prepareStatement(query);
-			// binding values
-
-			
-			ResultSet rs = preparedStmt.executeQuery();
-
-			JsonArray array = new JsonArray();
-			JsonObject innerProducts= new JsonObject();
-			while (rs.next()) {
-
-				innerProducts.addProperty("products_id", rs.getInt(1));
-				innerProducts.addProperty("research_id", rs.getInt(2));
-				innerProducts.addProperty("name", rs.getString(3));
-				innerProducts.addProperty("description", rs.getString(4));
-				innerProducts.addProperty("stock_quantity", rs.getInt(5));
-				innerProducts.addProperty("price", rs.getDouble(6));
-				innerProducts.addProperty("added_date", rs.getString(7));
-				array.add(innerProducts);
-
-			}
-			products.add("products", array);
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-			products.addProperty("status", "error");
-			return products.toString();
-
-		}
-
-		return products.toString();
-	}
 	
-	public String readProducts(int products_id) {
-		JsonObject products = new JsonObject();
-
-		try {
-
-			Connection con = connect();
-			if (con == null) {
-				return products.toString();
-			}
-			// create a prepared statement
-			String query = " select * from products where products_id = ? ";
-			PreparedStatement preparedStmt = con.prepareStatement(query);
-			// binding values
-
-			preparedStmt.setInt(1, products_id);
-			ResultSet rs = preparedStmt.executeQuery();
-
-			JsonArray array = new JsonArray();
-			JsonObject innerProducts = new JsonObject();
-			while (rs.next()) {
-
-				innerProducts.addProperty("research_id", rs.getInt(2));
-				innerProducts.addProperty("name", rs.getString(3));
-				innerProducts.addProperty("description", rs.getString(4));
-				innerProducts.addProperty("stock_quantity", rs.getInt(5));
-				innerProducts.addProperty("price", rs.getDouble(6));
-				innerProducts.addProperty("added_date", rs.getString(7));
-				array.add(innerProducts);
-
-			}
-			products.add("products", array);
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-			products.addProperty("status", "error");
-			return products.toString();
-
-		}
-
-		return products.toString();
-	}
 	
-	public String removeProducts( int products_id) {
-		String output = "Error";
-		try {
-			Connection con = connect();
-			if (con == null) {
-				return "Error while connecting to the database";
-			}
-			// create a prepared statement
-			String query = " Delete FROM products where products_id = ? ";
-			PreparedStatement preparedStmt = con.prepareStatement(query);
-			// binding values
-
-			preparedStmt.setInt(1, products_id);
-		
-			// execute the statement
-
-			int result = preparedStmt.executeUpdate();
-
-			if (result >= 1) {
-				output = "Successfully Deleted";
-
-			}
-			con.close();
-
-		} catch (Exception e) {
-			output = "Exception Occurred";
-			System.err.println(e.getMessage());
-		}
-		return output;
-	}
-	
-	public String updateItem (int products_id, int research_id, String name, String description, int stock_quantity, double price, String added_date){
-		 
-		String output = "";
-		try{
-			 Connection con = connect();
-			 if (con == null){
-				 return "Error while connecting to the database for updating."; 
-			 }
-			 
-			 // create a prepared statement
-			 String query = "UPDATE products SET research_id=?,name=?,description=?,stock_quantity=?,price=?,added_date=? WHERE products_id=?";
-			 PreparedStatement preparedStmt = con.prepareStatement(query);
-			 
-			 // binding values
-			 preparedStmt.setInt(1, research_id);
-			 preparedStmt.setString(2, name);
-			 preparedStmt.setString(3, description);
-			 preparedStmt.setInt(4, stock_quantity);
-			 preparedStmt.setDouble(5, price);
-			 preparedStmt.setString(6, added_date);
-			 preparedStmt.setInt(7, products_id);
-			 
-			 // execute the statement
-			 preparedStmt.execute();
-			 con.close();
-			 output = "Updated successfully";
-			 
-		 }
-		 catch (Exception e)
-		 {
-			 output = "Error while updating the item.";
-			 System.err.println(e.getMessage());
-		 }
-		 return output;
-		 }
 
 }
