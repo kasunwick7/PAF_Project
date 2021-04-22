9e6945692ffd33f4939880b89cdb4b9aff192afa
kasunwick7 <kasunravilal@gmail.com>
2021-04-22 07:15:25 +0530

    done

--- Product Service/REST/src/model/ProductManagementModel.java
+++ Product Service/REST/src/model/ProductManagementModel.java
@@ -3,6 +3,10 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class ProductManagementModel {
 	
@@ -11,7 +15,7 @@ public class ProductManagementModel {
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "1234");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/product_management_db", "root", "1234");
 		}
 		catch (Exception e)
 		{
@@ -19,7 +23,7 @@ public class ProductManagementModel {
 		return con;
 	}
 	
-	public boolean addProducts (int products_id, int research_id, String name, String description, int stock_quantity, double price, String added_date) {
+	public boolean addProducts (int research_id, String name, String description, int stock_quantity, double price, String added_date) {
 
 		try {
 
@@ -30,17 +34,17 @@ public class ProductManagementModel {
 			}
 			
 			// create a prepared statement
-			String query = " insert into products values(? , ? , ? , ? , ? , ? , ?) ";
+			String query =  " insert into products (`research_id`,`name`,`description`,`stock_quantity`,`price`,`added_date`) values (?, ?, ?, ?, ?, ?)"; 
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
 
-			preparedStmt.setInt(1, products_id);
-			preparedStmt.setInt(2, research_id);
-			preparedStmt.setString(3, name);
-			preparedStmt.setString(4, description);
-			preparedStmt.setInt(5, stock_quantity);
-			preparedStmt.setDouble(6, price);
-			preparedStmt.setString(7, added_date);
+			
+			preparedStmt.setInt(1, research_id);
+			preparedStmt.setString(2, name);
+			preparedStmt.setString(3, description);
+			preparedStmt.setInt(4, stock_quantity);
+			preparedStmt.setDouble(5, price);
+			preparedStmt.setString(6, added_date);
 			preparedStmt.executeUpdate();
 
 		} catch (Exception e) {
@@ -54,7 +58,159 @@ public class ProductManagementModel {
 	}
 	
 	
+	public String readProducts() {
+		JsonObject products = new JsonObject();
+		System.out.println("aaaaa");
+		try {
+
+			Connection con = connect();
+			if (con == null) {
+				return products.toString();
+			}
+			// create a prepared statement
+			String query = " select * from products ";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+
+			
+			ResultSet rs = preparedStmt.executeQuery();
+
+			JsonArray array = new JsonArray();
+			JsonObject innerProducts= new JsonObject();
+			while (rs.next()) {
+
+				innerProducts.addProperty("products_id", rs.getInt(1));
+				innerProducts.addProperty("research_id", rs.getInt(2));
+				innerProducts.addProperty("name", rs.getString(3));
+				innerProducts.addProperty("description", rs.getString(4));
+				innerProducts.addProperty("stock_quantity", rs.getInt(5));
+				innerProducts.addProperty("price", rs.getDouble(6));
+				innerProducts.addProperty("added_date", rs.getString(7));
+				array.add(innerProducts);
+
+			}
+			products.add("products", array);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			products.addProperty("status", "error");
+			return products.toString();
+
+		}
+
+		return products.toString();
+	}
 	
+	public String readProducts(int products_id) {
+		
+		JsonObject products = new JsonObject();
+
+		try {
+
+			Connection con = connect();
+			if (con == null) {
+				return products.toString();
+			}
+			// create a prepared statement
+			String query = " select * from products where products_id = ? ";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+
+			preparedStmt.setInt(1, products_id);
+			ResultSet rs = preparedStmt.executeQuery();
+
+			JsonArray array = new JsonArray();
+			JsonObject innerProducts = new JsonObject();
+			while (rs.next()) {
+
+				innerProducts.addProperty("research_id", rs.getInt(2));
+				innerProducts.addProperty("name", rs.getString(3));
+				innerProducts.addProperty("description", rs.getString(4));
+				innerProducts.addProperty("stock_quantity", rs.getInt(5));
+				innerProducts.addProperty("price", rs.getDouble(6));
+				innerProducts.addProperty("added_date", rs.getString(7));
+				array.add(innerProducts);
+
+			}
+			products.add("products", array);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			products.addProperty("status", "error");
+			return products.toString();
+
+		}
+
+		return products.toString();
+	}
 	
+	public String removeProducts( int products_id) {
+		String output = "Error";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database";
+			}
+			// create a prepared statement
+			String query = " Delete FROM products where products_id = ? ";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+
+			preparedStmt.setInt(1, products_id);
+		
+			// execute the statement
+
+			int result = preparedStmt.executeUpdate();
+
+			if (result >= 1) {
+				output = "Successfully Deleted";
+
+			}
+			con.close();
+
+		} catch (Exception e) {
+			output = "Exception Occurred";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	public String updateProduct(int products_id, int research_id, String name, String description, int stock_quantity, double price, String added_date){
+		 
+		String output = "";
+		try{
+			 Connection con = connect();
+			 if (con == null){
+				 return "Error while connecting to the database for updating."; 
+			 }
+			 
+			 // create a prepared statement
+			 String query = "UPDATE products SET research_id=?,name=?,description=?,stock_quantity=?,price=?,added_date=? WHERE products_id=?";
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 preparedStmt.setInt(1, research_id);
+			 preparedStmt.setString(2, name);
+			 preparedStmt.setString(3, description);
+			 preparedStmt.setInt(4, stock_quantity);
+			 preparedStmt.setDouble(5, price);
+			 preparedStmt.setString(6, added_date);
+			 preparedStmt.setInt(7, products_id);
+			 
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 output = "Updated successfully";
+			 
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while updating the item.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output;
+		 }
 
 }
